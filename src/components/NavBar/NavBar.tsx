f4ec3b73766007fd7ee053a5212c06d8615ab644
Elio Cortés <elio.cortes3000@gmail.com>
2025-02-14 23:06:15 -0500

    Fixed navbar issue

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,17 +1,26 @@
-
 import React from 'react';
 import styles from './NavBar.module.css';
 
 const NavBar: React.FC = () =>
 {
+    const handleClick = (e: React.MouseEvent, id: string) =>
+    {
+        e.preventDefault();
+        const element = document.getElementById(id);
+        if (element)
+        {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     return (
         <nav className={styles.navbar}>
             <ul>
-                <li><a href="#home">Inicio</a></li>
-                <li><a href="#about">Sobre Nosotros</a></li>
-                <li><a href="#services">Servicios</a></li>
-                <li><a href="#products">Productos</a></li>
-                <li><a href="#contact">Contacto</a></li>
+                <li><a href="#home" onClick={(e) => handleClick(e, 'home')}>Inicio</a></li>
+                <li><a href="#about" onClick={(e) => handleClick(e, 'about')}>Sobre Nosotros</a></li>
+                <li><a href="#services" onClick={(e) => handleClick(e, 'services')}>Servicios</a></li>
+                <li><a href="#products" onClick={(e) => handleClick(e, 'products')}>Productos</a></li>
+                <li><a href="#contact" onClick={(e) => handleClick(e, 'contact')}>Contacto</a></li>
             </ul>
         </nav>
     );
