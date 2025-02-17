1a7f1de5a0274d6dc421db77d5d7145d1e51dcdc
Elio Cortés <elio.cortes3000@gmail.com>
2025-02-16 22:27:49 -0500

    added image folder

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './Home.module.css';
 import LoremIpsum from 'react-lorem-ipsum';
-
+import coffeePlant from '/assets/images/coffee-plant.jpg';
 const Home: React.FC = () =>
 {
     return (
         <section id="home" className={styles.home}>
             <h2>Home</h2>
-            <img src="coffee-plant.jpg" alt="Mata de café" className={styles.banner} />
+            <img src={coffeePlant} alt="Planta de café" className={styles.banner} />
             <LoremIpsum p={5} />
         </section>
     );
